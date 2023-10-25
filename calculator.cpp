8df66c4fe15051770c9278429507ba35e57c8296
a_lupaschenko_io14 <artem.lapa03@gmail.com>
2023-10-25 23:09:55 +0300

    Added division function

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,13 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+int Calculator::Div (double a, double b)
+{
+    if (b != 0) {
+        return a / b + 0.5;
+    } else {
+        return 0;
+    }
+}
+
