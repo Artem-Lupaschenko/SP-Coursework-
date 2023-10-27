2a501ea5d47b9d8dc86d678f4738a1e18cb44a55
Artem-Lupaschenko <artem.lapa03@gmail.com>
2023-10-27 13:33:23 +0300

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "calculator.h"
+#include <iostream>
+
+int main() {
+    Calculator calc;
+
+    // Використовуємо сталі значення для тестування
+    double num1 = 10.5;
+    double num2 = 5.2;
+
+    int result_add = calc.Add(num1, num2);
+    int result_sub = calc.Sub(num1, num2);
+    int result_mul = calc.Mul(num1, num2);
+    int result_div = calc.Div(num1, num2);
+
+    std::cout << "Addition result: " << result_add << std::endl;
+    std::cout << "Subtraction result: " << result_sub << std::endl;
+    std::cout << "Multiplication result: " << result_mul << std::endl;
+    std::cout << "Division result: " << result_div << std::endl;
+
+    return 0;
+}
